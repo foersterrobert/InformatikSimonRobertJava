7c402c887f7774325d47a379b929cc26d3788093
robertfoerster <foersteroberthh@gmail.com>
2021-12-13 12:14:54 +0100

    kinderTicket

--- Robert/Ticketautomat.java
+++ Robert/Ticketautomat.java
@@ -3,29 +3,29 @@ import java.awt.event.*;
 
 public class Ticketautomat
 {
-    static private Ticket[] tickets;
     static private int bisherGezahlt;
     static private int summe;
+    Ticket kinderTicket = new Ticket(160, "Ticket-Kinder");
+    Ticket normalTicket = new Ticket(240, "Ticket-Erwachsene");
+    Ticket gruppenTicket = new Ticket(320, "Ticket-Gruppen");
 
     public Ticketautomat()
     {
-        tickets = new Ticket[3];
-        for (int i = 0; i<tickets.length; i++) {
-            tickets[i] = new Ticket(100*(i+1), "Line " + i);
-        }
         bisherGezahlt = 0;
         summe = 0;
 
         JFrame frame = new JFrame("Ticketautomat");
         JPanel container = new JPanel();
         JLabel label = new JLabel("Ticketautomat");
+        JMenu ticketMenu = new JMenu("Tickets");
+
         JLabel bisherGezahltLabel = new JLabel("Bisher gezahlt: " + bisherGezahlt);
-        JLabel ticketPreisLabel = new JLabel("Ticketpreis: " + tickets[0].ticketPreis);
+        JLabel ticketPreisLabel = new JLabel("Ticketpreis: " + kinderTicket.ticketPreis);
         JTextField input = new JTextField(30);
         JButton button = new JButton("Einwerfen");
 
         JDialog ticketDialog = new JDialog();
-        JLabel ticketLabel = new JLabel("Ticket: " + tickets[0].ticketName);
+        JLabel ticketLabel = new JLabel("Ticket: " + kinderTicket.ticketName);
         JLabel wechseLabel = new JLabel("Wechselgeld: " + summe);
         
         button.addActionListener(new ActionListener(){
@@ -36,7 +36,7 @@ public class Ticketautomat
                 input.setText("");
                 bisherGezahltLabel.setText("Bisher gezahlt: " + bisherGezahlt);
                 if (wechselGeld >= 0) {
-                    JOptionPane.showMessageDialog(frame, "Ticket " + tickets[0].ticketName + " gedruckt. \n Wechselgeld: " + wechselGeld, "Ticket gedruckt", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(frame, kinderTicket.ticketName + " gedruckt. \n Wechselgeld: " + wechselGeld, "Ticket gedruckt", JOptionPane.INFORMATION_MESSAGE);
                 }
             }
         });
@@ -45,6 +45,7 @@ public class Ticketautomat
         frame.setSize(500, 600);
 
         container.add(label);
+        container.add(ticketMenu);
         container.add(bisherGezahltLabel);
         container.add(ticketPreisLabel);
         container.add(input);
@@ -55,33 +56,19 @@ public class Ticketautomat
 
         frame.add(container);
     }
-
-    public void printPreis()
-    {
-        System.out.println(tickets[0].ticketPreis + " cent");
-    }
     
-    public void printBisherGezahlt()
+    public int GeldEinwerfen(int betrag)
     {
-        // put your code here
-        System.out.println(bisherGezahlt + " cent");
-    }
-    
-    static public int GeldEinwerfen(int betrag)
-    {
-        // put your code here
         bisherGezahlt += betrag;
         summe += betrag;
-        if (bisherGezahlt >= tickets[0].ticketPreis) {
-            bisherGezahlt -= tickets[0].ticketPreis;
+        if (bisherGezahlt >= kinderTicket.ticketPreis) {
+            bisherGezahlt -= kinderTicket.ticketPreis;
             int wechselgeld = bisherGezahlt;
             bisherGezahlt = 0;
             return wechselgeld;
         }
-
         return -1;
     }
-    
 
     public static void main(String[]arg){
         Ticketautomat automat = new Ticketautomat();
