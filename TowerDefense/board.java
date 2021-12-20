a70ff5e197e31271d41ad3d63eb1cc5cf940b74c
robertfoerster <foersteroberthh@gmail.com>
2021-12-20 18:15:18 +0100

    TowerDefenseJava

--- TowerDefense/board.java
+++ TowerDefense/board.java
@@ -1,15 +1,78 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class board extends JFrame {
+public class board extends JFrame implements MouseListener {
     JPanel panel = new JPanel();
-    // add image to jpanel
-    JLabel label = new JLabel(new ImageIcon("assets/bg.jpeg"));
+    cell[][] cells = new cell[10][10];
 
     public board() {
-        setSize(500, 500);
-        // resize label
-        panel.add(label);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                cells[i][j] = new cell(i, j, 50, 50);
+                if (i % 2 == 0) {
+                    cells[i][j].color = Color.CYAN;
+                }
+            }
+        }
+        cells[0][0].color = Color.RED; 
+        setSize(500, 525);
         add(panel);
         setVisible(true);
+        addMouseListener(this);
+    }
+
+    public void mousePressed(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        System.out.println("Clicked at " + x + "," + y);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (x > i * 50 && x < (i + 1) * 50 && y > j * 50 + 25 && y < (j + 1) * 50 + 25) {
+                    if (cells[i][j].color == Color.BLACK) {
+                        cells[i][j].color = Color.RED;
+                        cells[i][j].draw(panel.getGraphics());
+                    }
+                }
+            }
+        }
+        repaint();
+    }
+
+    public void paint(Graphics g) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                cells[i][j].draw(g);
+            }
+        }
+    }
+    
+
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+        // TODO Auto-generated method stub
+        
     }
 }
---
+++ TowerDefense/cell.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+
+public class cell {
+    int x;
+    int y;
+    int width;
+    int height;
+    Color color = Color.BLACK;
+
+    public cell(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void hover() {
+        System.out.println("Hovering over cell" + x + "," + y);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(this.color);
+        g.fillRect(x * width, y * height + 25, width, height);
+        g.setColor(Color.WHITE);
+        g.drawRect(x*width, y*height + 25, width, height);
+    }
+}
