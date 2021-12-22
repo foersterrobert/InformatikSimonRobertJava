b8d061cb2edbf8c7d3feefa216634a484acb3dd8
HowToCarryyy <howtobecarried@web.de>
2021-12-22 17:04:09 +0100

    update

--- TowerDefense/Main.java
+++ TowerDefense/Main.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         // Create a new board
-        new board();
+        board board = new board();
+
+        javaEnemy javaEnemy = new javaEnemy();
+        
     }
 }
