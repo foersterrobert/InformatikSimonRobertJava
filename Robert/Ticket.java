f5db59555694738f13f6120cd25dc092040e2a42
Robert <foersteroberthh@gmail.com>
2022-01-12 20:49:09 +0100

    TicketSortiert, Settings, Icon, Menu

--- Robert/Ticket.java
+++ Robert/Ticket.java
@@ -1,13 +1,10 @@
-
 public class Ticket
 {
-    // instance variables - replace the example below with your own
     public int ticketPreis;
     public String ticketName;
 
     public Ticket(int _ticketPreis, String _ticketName)
     {
-        // initialise instance variables
         ticketPreis = _ticketPreis;
         ticketName = _ticketName;
     }
--- Robert/Ticketautomat.java
+++ Robert/Ticketautomat.java
@@ -3,56 +3,88 @@ import java.awt.event.*;
 
 public class Ticketautomat
 {
-    static private int bisherGezahlt;
-    static private int summe;
-    Ticket kinderTicket = new Ticket(160, "Ticket-Kinder");
-    Ticket normalTicket = new Ticket(240, "Ticket-Erwachsene");
-    Ticket gruppenTicket = new Ticket(320, "Ticket-Gruppen");
+    int bisherGezahlt;
+    int summe;
+    Ticket[] tickets = {new Ticket(5, "Kinder-Ticket"), new Ticket(10, "Erwachsenen-Ticket"), new Ticket(15, "Studenten-Ticket"), new Ticket(20, "Gruppen-Ticket")};
+    JMenuItem[] menuitems = {new JMenuItem("Kinder-Ticket"), new JMenuItem("Erwachsenen-Ticket"), new JMenuItem("Studenten-Ticket"), new JMenuItem("Gruppen-Ticket")};
+    Ticket ticket;
 
     public Ticketautomat()
     {
         bisherGezahlt = 0;
         summe = 0;
+        ticket = tickets[0];
 
         JFrame frame = new JFrame("Ticketautomat");
         JPanel container = new JPanel();
         JLabel label = new JLabel("Ticketautomat");
+        JMenuBar menuBar = new JMenuBar();
         JMenu ticketMenu = new JMenu("Tickets");
 
-        JLabel bisherGezahltLabel = new JLabel("Bisher gezahlt: " + bisherGezahlt);
-        JLabel ticketPreisLabel = new JLabel("Ticketpreis: " + kinderTicket.ticketPreis);
-        JTextField input = new JTextField(30);
-        JButton button = new JButton("Einwerfen");
+        ImageIcon icon = new ImageIcon("icon.png");
 
-        JDialog ticketDialog = new JDialog();
-        JLabel ticketLabel = new JLabel("Ticket: " + kinderTicket.ticketName);
-        JLabel wechseLabel = new JLabel("Wechselgeld: " + summe);
+        JLabel bisherGezahltLabel = new JLabel("Bisher gezahlt: " + bisherGezahlt + "€");
+        JLabel ticketLabel = new JLabel("Ticket: " + ticket.ticketName);
+        JLabel ticketPreisLabel = new JLabel("Ticketpreis: " + ticket.ticketPreis + "€");
+        JTextField ticketInput = new JTextField(30);
+        JButton einwerfenBtn = new JButton("Einwerfen");
+        JButton zurueckgebenBtn = new JButton("Zurückgeben");
+        JButton settingsBtn = new JButton("Einstellungen ⚙️");
+
+        JDialog settingsDialog = new JDialog();
         
-        button.addActionListener(new ActionListener(){
+        einwerfenBtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                String inputSummeString = input.getText();
+                String inputSummeString = ticketInput.getText();
                 int inputSummeInt = Integer.parseInt(inputSummeString);
                 int wechselGeld = GeldEinwerfen(inputSummeInt);
-                input.setText("");
-                bisherGezahltLabel.setText("Bisher gezahlt: " + bisherGezahlt);
+                ticketInput.setText("");
+                bisherGezahltLabel.setText("Bisher gezahlt: " + bisherGezahlt + "€");
                 if (wechselGeld >= 0) {
-                    JOptionPane.showMessageDialog(frame, kinderTicket.ticketName + " gedruckt. \n Wechselgeld: " + wechselGeld, "Ticket gedruckt", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(frame, ticket.ticketName + " gedruckt. \n Wechselgeld: " + wechselGeld + "€", "Ticket gedruckt", JOptionPane.INFORMATION_MESSAGE);
                 }
+            }
+        });
+
+        settingsBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                settingsDialog.setTitle("Einstellungen");
+                settingsDialog.setSize(300, 200);
+                settingsDialog.setLocationRelativeTo(frame);
+                settingsDialog.setVisible(true);
+            }
+        });
+
+        zurueckgebenBtn.addActionListener(l -> {
+            int wechselGeld = GeldZurueckgeben();
+            bisherGezahltLabel.setText("Bisher gezahlt: " + bisherGezahlt + "€");
+            if (wechselGeld >= 0) {
+                JOptionPane.showMessageDialog(frame, "Wechselgeld: " + wechselGeld + "€", "Wechselgeld zurückgegeben", JOptionPane.INFORMATION_MESSAGE);
             }
         });
         
         frame.setVisible(true);
+        frame.setTitle("Ticketautomat");
+        frame.setIconImage(icon.getImage());
         frame.setSize(500, 600);
 
-        container.add(label);
-        container.add(ticketMenu);
-        container.add(bisherGezahltLabel);
-        container.add(ticketPreisLabel);
-        container.add(input);
-        container.add(button);
+        for (int i = 0; i < menuitems.length; i++) {
+            ticketMenu.add(menuitems[i]);
+        }
 
-        ticketDialog.add(ticketLabel);
-        ticketDialog.add(wechseLabel);
+        menuBar.add(ticketMenu);
+
+        container.add(label);
+        container.add(menuBar);
+        container.add(bisherGezahltLabel);
+        container.add(ticketLabel);
+        container.add(ticketPreisLabel);
+        container.add(ticketInput);
+        container.add(einwerfenBtn);
+        container.add(settingsBtn);
+        container.add(zurueckgebenBtn);
+
+        settingsDialog.setIconImage(icon.getImage());
 
         frame.add(container);
     }
@@ -61,16 +93,17 @@ public class Ticketautomat
     {
         bisherGezahlt += betrag;
         summe += betrag;
-        if (bisherGezahlt >= kinderTicket.ticketPreis) {
-            bisherGezahlt -= kinderTicket.ticketPreis;
-            int wechselgeld = bisherGezahlt;
-            bisherGezahlt = 0;
-            return wechselgeld;
+        if (bisherGezahlt >= ticket.ticketPreis) {
+            bisherGezahlt -= ticket.ticketPreis;
+            return GeldZurueckgeben();
         }
         return -1;
     }
 
-    public static void main(String[]arg){
-        Ticketautomat automat = new Ticketautomat();
+    public int GeldZurueckgeben()
+    {
+        int wechselgeld = bisherGezahlt;
+        bisherGezahlt = 0;
+        return wechselgeld;
     }
 }
