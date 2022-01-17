509fd456e701fda94149b594f192c95416b018a9
HowToCarryyy <howtobecarried@web.de>
2022-01-17 19:06:53 +0100

    Update Fetch.java

--- Wetter/Fetch.java
+++ Wetter/Fetch.java
@@ -7,13 +7,12 @@ public class Fetch {
     public Fetch() throws IOException{
         // String content = getUrlAsString("https://www.google.com");
         // System.out.println(content);
-        URL website = new URL("http://api.openweathermap.org/data/2.5/weather?id=2911293&appid=92c52fc8f6b4286b0a9b8106b4568820&cnt=5&units=metric&lang=de");
+        URL website = new URL("https://api.openweathermap.org/data/2.5/weather?q=eimsbuettel&appid=92c52fc8f6b4286b0a9b8106b4568820&cnt=5&units=metric&lang=de");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(website.openStream()));
 
         String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+        while ((inputLine = in.readLine()) != null) System.out.println(inputLine);
         in.close();
     }
 }
